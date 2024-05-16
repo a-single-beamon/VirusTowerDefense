5272f30685b9d54d66de45695ad8d6d0f9949ddf
Villanuev-a <jhvmalavazi@gmail.com>
2024-05-16 14:23:43 -0300

    add telas login

---
+++ immunity_defense/src/main/java/com/immunity_defense/immunity_defense/Immunity_defense.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.immunity_defense.immunity_defense;
+
+/**
+ *
+ * @author joseh
+ */
+public class Immunity_defense {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
